5f114581c880482799c5d747542c2665215dba58
Alex Tran <tranduclong092@gmail.com>
2024-09-13 10:48:05 +0300

    Feat: modify country cards

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { initializeCountries } from "../services/countriesServices";
-import { Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import { Col, Container, Form, ListGroup, ListGroupItem, Row, Spinner } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { search } from "../store/countriesSlice";
+import { Link } from "react-router-dom";
 
 export default function Countries() {
   const dispatch = useDispatch();
@@ -13,8 +14,7 @@ export default function Countries() {
   const isLoading = useSelector(state => state.countries.isLoading);
   const searchInput = useSelector(state => state.countries.search);
 
-  console.log(searchInput);
-  // console.log(countries);
+  console.log(countries);
 
   useEffect(() => {
     dispatch(initializeCountries())
@@ -36,6 +36,7 @@ export default function Countries() {
 
   return (
     <Container fluid>
+      {/* search bar */}
       <Row className="gap-5 m-1">
         <Col className="mt-5 d-flex justify-content-center">
           <Form>
@@ -49,18 +50,32 @@ export default function Countries() {
             />
           </Form>
         </Col>
-        {countries.map(country => (
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={country.flags.svg} />
-            <Card.Body>
-              <Card.Title>{country.name.common}</Card.Title>
-              <Card.Text>{country.name.official}</Card.Text>
-              <Button variant="primary">Read more {country.flag}</Button>
-            </Card.Body>
-          </Card>
-        ))
-        }
       </Row>
-    </Container>
+      {/* country cards */}
+      <Row xs={2} md={3} lg={4} className="g-3">
+        {countries.map(country => (
+          <Card className="h-100" key={country.cca3}>
+            <Link className="link-underline link-underline-opacity-0">
+              <Card.Img
+                variant="top"
+                src={country.flags.svg}
+                alt={`${country.name.common} flag`}
+                className="rounded h-50"
+                style={{
+                  objectFit: "cover",
+                  minHeight: "200px",
+                  maxHeight: "200px"
+                }}
+              />
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{country.name.common}</Card.Title>
+                <Card.Subtitle className="mb-5 text-muted">{country.name.official}</Card.Subtitle>
+                <Button variant="primary">Read more {country.flag}</Button>
+              </Card.Body>
+            </Link>
+          </Card>
+        ))}
+      </Row>
+    </Container >
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
