e79174b03c4e7c491e5b4c8c94a2d067e4fa1047
Alex Tran <tranduclong092@gmail.com>
2024-09-13 13:59:45 +0300

    Feat: update weather display styling

--- src/components/SingleCountry.jsx
+++ src/components/SingleCountry.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card, Col, ListGroup, Spinner } from "react-bootstrap";
+import { Card, Col, Container, ListGroup, Row, Spinner } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
@@ -23,7 +23,8 @@ export default function SingleCountry() {
         // pass the latitude and longtitude of the capital
         + `lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}`
         // pass other parameters and api key
-        + `&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`)
+        + `&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`
+      )
       .catch(error => console.log(error))
       .then(res => {
         setWeather(res.data);
@@ -54,49 +55,53 @@ export default function SingleCountry() {
 
   return (
     <>
-      <Card className="h-100">
-        <Card.Img
-          variant="top"
-          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-          alt="weather icon"
-          className="rounded h-50"
-          style={{
-            objectFit: "cover",
-            minHeight: "200px",
-            maxHeight: "50px",
-            maxWidth: "200px"
-          }}
-        />
-        <Card.Body className="d-flex flex-column">
-          <Card.Title>{weather.name}</Card.Title>
-          <Card.Subtitle className="mb-5 text-muted">
-            {dateTimeString}<br />
-            {capitalized(weather.weather[0].description)}
-          </Card.Subtitle>
-          <ListGroup variant="flush" className="flex-grow-1 justify-content-center">
-            {/* temperature */}
-            <ListGroup.Item>
-              <i className="bi bi-tree me-2"> Temperature: {roundTemp(weather.main.temp)}&deg;C</i>
-            </ListGroup.Item>
-            {/* feel like */}
-            <ListGroup.Item>
-              <i className="bi bi-geo me-2"> Feel like: {roundTemp(weather.main.feels_like)}&deg;C</i>
-            </ListGroup.Item>
-            {/* temp high */}
-            <ListGroup.Item>
-              <i className="bi bi-people me-2"> High: {roundTemp(weather.main.temp_max)}&deg;C</i>
-            </ListGroup.Item>
-            {/* temp low */}
-            <ListGroup.Item>
-              <i className="bi bi-people me-2"> Low: {roundTemp(weather.main.temp_min)}&deg;C</i>
-            </ListGroup.Item>
-            {/* temp low */}
-            <ListGroup.Item>
-              <i className="bi bi-people me-2"> Humidity: {weather.main.humidity}</i>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
+      <Container fluid className="p-5">
+        <Row className="d-flex flex-column align-items-center">
+          <Col className="mt-5 d-flex flex-column justify-content-center">
+            <Card.Img
+              variant="top"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt="weather icon"
+              className="rounded h-50"
+              style={{
+                objectFit: "cover",
+                minHeight: "200px",
+                maxHeight: "50px",
+                maxWidth: "200px"
+              }}
+            />
+            <Card.Title>{weather.name}</Card.Title>
+            <Card.Subtitle className="mb-5 text-muted">
+              {dateTimeString}<br />
+              {capitalized(weather.weather[0].description)}
+            </Card.Subtitle>
+          </Col>
+          <Col>
+            <ListGroup variant="flush" className="flex-grow-1 justify-content-center">
+              {/* temperature */}
+              <ListGroup.Item>
+                <i className="bi bi-thermometer-half me-2 w-10"> Temperature: {roundTemp(weather.main.temp)}&deg;C</i>
+              </ListGroup.Item>
+              {/* feel like */}
+              <ListGroup.Item>
+                <i className="bi bi-emoji-smile me-2"> Feel like: {roundTemp(weather.main.feels_like)}&deg;C</i>
+              </ListGroup.Item>
+              {/* temp high */}
+              <ListGroup.Item>
+                <i className="bi bi-thermometer-sun me-2"> High: {roundTemp(weather.main.temp_max)}&deg;C</i>
+              </ListGroup.Item>
+              {/* temp low */}
+              <ListGroup.Item>
+                <i className="bi bi-thermometer-snow me-2"> Low: {roundTemp(weather.main.temp_min)}&deg;C</i>
+              </ListGroup.Item>
+              {/* temp low */}
+              <ListGroup.Item>
+                <i className="bi bi-moisture me-2"> Humidity: {weather.main.humidity}</i>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
