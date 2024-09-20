28417cea2aabb7e00dec49dc8285aabaabc91943
Alex Tran <tranduclong092@gmail.com>
2024-09-20 10:59:31 +0300

    Feat: add state and hooks for register component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, registerWithEmailAndPassword } from "../auth/firebase";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  // this hook handle auth state from firebase
+  const [user, loading, error] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    if (!name) {
+      alert("Name is required");
+    }
+    registerWithEmailAndPassword(name, email, password);
+  }
+
+  // TODO: Add a check to see if user if logged in and navigate to countries
+
   return (
-    <div>Register</div>
+    <div>
+      form here
+    </div>
   )
 }
