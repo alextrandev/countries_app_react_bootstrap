627b653d30911225339b688d2a7b755050c24167
Alex Tran <tranduclong092@gmail.com>
2024-09-20 12:26:46 +0300

    Feat: create login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,43 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, loginWithEmailAndPassword } from "../auth/firebase";
+import { useState } from "react";
+import { Button } from 'react-bootstrap';
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+
+  const handleLogin = () => {
+    if (!email) {
+      alert("Email is required");
+      return;
+    } else if (!password) {
+      alert("Password is required");
+      return;
+    }
+    loginWithEmailAndPassword(email, password);
+  }
 
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <Button
+        onClick={handleLogin}
+      >
+        Login
+      </Button>
+    </div>
   )
 }
