b063ba269f6c19b0e6809a1c486f7a11f6d0c1fe
Alex Tran <tranduclong092@gmail.com>
2024-10-07 10:55:31 +0300

    Feat: improve ai function and add error handling

--- src/services/aiTriviaService.js
+++ src/services/aiTriviaService.js
@@ -1,13 +1,14 @@
 import OpenAI from "openai";
 import sampleResponse from "../lib/sampleAiTriviaResponse.json";
+import { toast } from "react-toastify";
 
 const client = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 
-export default async function fetchTrivials({cca3}) {
+export default async function fetchTrivials(cca3) {
   // prepare the question prompt for AI
-  const prompt = `Generate 15 distinct trivial facts on the country with cca3 code ${cca3} and ensure they are in JSON format containing an id and the fact string. Some extra requirement are:
+  const prompt = `Generate 15 distinct trivial facts on the country with cca3 code ${cca3} and ensure they are in JSON format containing an id and the fact string. Some extra requirements are:
     - Don't make the fact too obvious and lengthy.
     - Ensure the facts are unique and not repetitive.
     - The fact should not be too simple but intermediate level.
@@ -15,21 +16,38 @@ export default async function fetchTrivials({cca3}) {
     You can use this as a sample response: ${JSON.stringify(sampleResponse)}
   `;
 
-  // generate question in the required format
-  const completion = await client.chat.completions.create({
-    messages: [{role: 'user', content: prompt}],
-    model: 'gpt-3.5-turbo',
-  });
+  try {
+    // generate question in the required format
+    const completion = await client.chat.completions.create({
+      messages: [{role: 'user', content: prompt}],
+      model: 'gpt-3.5-turbo',
+    });
 
   // process the response from AI
-  const res = completion.choices[0].message.content;
-  const trivials = JSON.parse(res);
+    const res = completion?.choices?.[0]?.message?.content;
 
-  // error catching
-  if (trivials.length < 15) {
-    // to do
+    // errors handling
+    if (!res) {
+      console.log("Error", completion);
+      throw new Error("No response from OpenAI");
+    }
+
+    try {
+    const trivials = JSON.parse(res);
+    } catch (parseError) {
+      console.log('Error parsing AI response', parseError.message);
+      throw new Error("Failed to process OpenAI response");
+    }
+
+    if (!Array.isArray(trivials) || trivials.length < 15) {
+      console.log(`Error: expected 15 facts, recieved ${trivials.length}`);
+      throw new Error("AI failed to provive a response");
+    }
+
+    // return the trivia facts
+    return trivials;
+
+  } catch (error) {
+    toast.error(error.message);
   }
-
-  // return the responsed trivials
-  return trivials;
 }
