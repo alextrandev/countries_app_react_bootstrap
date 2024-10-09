77bd1e07352a1c2372b978c7234bbe5f7de9426d
Alex Tran <tranduclong092@gmail.com>
2024-10-09 12:53:04 +0300

    Feat: add pagination bar

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,29 @@
+import Pagination from 'react-bootstrap/Pagination';
 
-export default function Pagination() {
+export default function _Pagination() {
+  const active = 1;
+  const count = 10;
+  let items = [];
+
+  for (let number = 1; number <= count; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === active}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
   return (
-    <div>Pagination</div>
-  )
+    <Pagination>
+      <Pagination.First />
+      <Pagination.Prev />
+      {items[0]}
+      <Pagination.Ellipsis />
+      {items.slice(1, -1)}
+      <Pagination.Ellipsis />
+      {items.slice(-1)}
+      <Pagination.Next />
+      <Pagination.Last />
+    </Pagination>
+  );
 }
