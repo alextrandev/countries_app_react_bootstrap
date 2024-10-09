7de1ca9c3f907151c7093e9a17e527cbaf7588c7
Alex Tran <tranduclong092@gmail.com>
2024-10-09 14:06:20 +0300

    Feat: write logic for the pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,41 @@
+import { useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 
 export default function _Pagination() {
-  const active = 1;
-  const count = 10;
-  let items = [];
+  const [active, setActive] = useState(1);
+  const count = 300; // Total number of pages
 
-  for (let number = 1; number <= count; number++) {
-    items.push(
-      <Pagination.Item key={number} active={number === active}>
+  const Item = ({ number }) => {
+    return (
+      <Pagination.Item
+        key={number}
+        active={active === number}
+        onClick={() => setActive(number)}
+      >
         {number}
-      </Pagination.Item>,
+      </Pagination.Item>
+    )
+  }
+
+
+  {/* Show items based on the logic */ }
+  let start, end;
+  if (active < 5) {
+    start = 2;
+    end = 6;
+  } else if (active > count - 4) {
+    start = count - 5;
+    end = count - 1;
+  } else {
+    start = active - 2;
+    end = active + 2;
+  }
+
+  // this array hold the current shown item based on currently active
+  let displayItems = [];
+  for (let number = start; number <= end; number++) {
+    displayItems.push(
+      <Item number={number} />
     );
   }
 
@@ -17,11 +43,21 @@ export default function _Pagination() {
     <Pagination>
       <Pagination.First />
       <Pagination.Prev />
-      {items[0]}
-      <Pagination.Ellipsis />
-      {items.slice(1, -1)}
-      <Pagination.Ellipsis />
-      {items.slice(-1)}
+
+      {/* The first item should always be shown */}
+      <Item number={1} />
+
+      {/* show the Ellipsis when active is not the first 4 */}
+      {active > 4 && <Pagination.Ellipsis disabled />}
+
+      {displayItems}
+
+      {/* show the Ellipsis when active is not the last 4 */}
+      {active < count - 3 && <Pagination.Ellipsis disabled />}
+
+      {/* The last item should always be shown */}
+      <Item number={count} />
+
       <Pagination.Next />
       <Pagination.Last />
     </Pagination>
