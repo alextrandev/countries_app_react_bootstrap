02e45c926189fa4a0f1d155e87a8287805d984e3
Alex Tran <tranduclong092@gmail.com>
2024-10-10 09:45:18 +0300

    Refactor: export the navbar as functional component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,52 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
-import { LinkContainer } from 'react-router-bootstrap';
-import { auth, logout } from '../auth/firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import Nav from './Nav';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
-  const [user] = useAuthState(auth);
-
   return (
     <Container fluid>
-      <Row>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container className="justify-content-end">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <LinkContainer to="/">
-                  <Nav.Link>Home</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/countries">
-                  <Nav.Link>Countries</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/favourites">
-                  <Nav.Link>Favorites</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/login" disabled={user}>
-                  <Nav.Link>Login</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/register" disabled={user}>
-                  <Nav.Link>Register</Nav.Link>
-                </LinkContainer>
-                {user &&
-                  <LinkContainer to="/login">
-                    <Nav.Link onClick={logout}>Logout</Nav.Link>
-                  </LinkContainer>
-                }
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-          <Container className="justify-content-end">
-            Welcome, {user ? user.email : "guest"}!
-          </Container>
-        </Navbar>
-      </Row>
+      <Nav />
       <Row>
         <Outlet />
       </Row>
---
+++ src/pages/Nav.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
+import { auth, logout } from '../auth/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Row } from 'react-bootstrap';
+
+export default function _Nav() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <Row className='shadow sticky-top'>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container className="justify-content-start">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/countries">
+                <Nav.Link>Countries</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/favourites">
+                <Nav.Link>Favorites</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login" disabled={user}>
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register" disabled={user}>
+                <Nav.Link>Register</Nav.Link>
+              </LinkContainer>
+              {user &&
+                <LinkContainer to="/login">
+                  <Nav.Link onClick={logout}>Logout</Nav.Link>
+                </LinkContainer>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+        <Container className="justify-content-end text-white">
+          Welcome, {user ? user.email : "guest"}!
+        </Container>
+      </Navbar>
+    </Row>
+  )
+}
