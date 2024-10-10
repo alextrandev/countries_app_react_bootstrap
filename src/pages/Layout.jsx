001e09e4f52c50073c4e5c8ded8cf49c33550883
Alex Tran <tranduclong092@gmail.com>
2024-10-10 10:05:03 +0300

    Feat: make the page layout take full height

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Nav from './Nav';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './Footer';
 
 const Layout = () => {
   return (
-    <Container fluid>
+    <Container fluid className="d-flex flex-column" style={{ minHeight: '100vh' }}>
       <Nav />
-      <Row>
+      <Container fluid className="flex-grow-1">
         <Outlet />
-      </Row>
+      </Container>
       <Footer />
     </Container>
   );
